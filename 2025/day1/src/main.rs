d07318985912d3dd69913a52850acaa2b397e1f6
lcian <17258265+lcian@users.noreply.github.com>
2025-12-01 21:56:28 +0100

    2025: day 1

--- 2025/day1/src/main.rs
+++ 2025/day1/src/main.rs
@@ -2,6 +2,77 @@
 #![no_main]
 
 use prelude::*;
-setup!();
+prelude!();
 
-fn main() {}
+fn main() {
+    problem1();
+    problem2();
+}
+
+fn problem1() {
+    let input = include_str!("input.txt");
+    let mut res = 0;
+    let mut pos: i64 = 50;
+
+    for line in input.lines() {
+        let (sign, count) = line.split_at(1);
+        let sign: i64 = match sign {
+            "L" => -1,
+            "R" => 1,
+            _ => unreachable!(),
+        };
+        let count: u64 = atoi(count);
+        pos = pos + (sign * count as i64);
+        if pos < 0 {
+            pos = 100 + pos;
+        }
+        pos = pos % 100;
+
+        if pos == 0 {
+            res += 1;
+        }
+    }
+    println_int(res);
+}
+
+fn problem2() {
+    let input = include_str!("input.txt");
+    let mut res = 0;
+    let mut pos: i64 = 50;
+
+    for line in input.lines() {
+        let (sign, count) = line.split_at(1);
+        let mut count: u64 = atoi(count);
+        match sign {
+            "L" => {
+                while count > 0 {
+                    let c = count.min(100);
+                    count -= c;
+
+                    let old_pos: i64 = pos as i64;
+                    pos -= c as i64;
+                    if (old_pos > 0 && pos <= 0) || (pos == -100) {
+                        res += 1;
+                    }
+                    if pos < 0 {
+                        pos = 100 + pos;
+                    }
+                }
+            }
+            "R" => {
+                while count > 0 {
+                    let c = count.min(100);
+                    count -= c;
+
+                    pos += c as i64;
+                    if pos >= 100 {
+                        pos = pos % 100;
+                        res += 1;
+                    }
+                }
+            }
+            _ => unreachable!(),
+        };
+    }
+    println_int(res);
+}
--- 2025/prelude/src/lib.rs
+++ 2025/prelude/src/lib.rs
@@ -3,7 +3,7 @@
 use core::arch::asm;
 
 #[macro_export]
-macro_rules! setup {
+macro_rules! prelude {
     () => {
         #[cfg(not(test))] // workaround for rust-analyzer
         mod panic_handler {
@@ -12,16 +12,16 @@ macro_rules! setup {
 
             #[panic_handler]
             fn handle_panic(info: &PanicInfo) -> ! {
-                print("panicked in ".as_bytes());
-                println(info.location().unwrap().file().as_bytes());
-                exit(1);
+                print("panicked in ");
+                println(info.location().unwrap().file());
+                exit(1)
             }
         }
 
         #[unsafe(no_mangle)]
         pub extern "C" fn _start() -> ! {
             main();
-            exit(0);
+            exit(0)
         }
     };
 }
@@ -37,23 +37,47 @@ pub fn exit(code: u32) -> ! {
     unreachable!();
 }
 
-pub fn write(fd: u64, slice: &[u8]) {
+pub fn write(fd: u64, s: &str) {
     unsafe {
         asm!(
             "syscall",
             in("rax") 1,
             in("rdi") fd,
-            in("rsi") slice.as_ptr(),
-            in("rdx") slice.len()
+            in("rsi") s.as_ptr(),
+            in("rdx") s.len()
         );
     }
 }
 
-pub fn print(slice: &[u8]) {
-    write(1, slice);
+pub fn print(s: &str) {
+    write(1, s);
 }
 
-pub fn println(slice: &[u8]) {
-    print(slice);
-    print(b"\n");
+pub fn println(s: &str) {
+    print(s);
+    print("\n");
+}
+
+pub fn println_int(x: u64) {
+    let x = itoa(x as u64);
+    let s = str::from_utf8(&x).unwrap().trim_start_matches('0');
+    let s = if s.is_empty() { "0" } else { s };
+    println(s);
+}
+
+pub fn atoi(s: &str) -> u64 {
+    let mut res = 0;
+    for d in s.chars() {
+        res = res * 10 + (d as u64 - 48);
+    }
+    res
+}
+
+pub fn itoa(mut x: u64) -> [u8; 20] {
+    let mut res: [u8; 20] = [0; 20];
+    for i in (0..20).rev() {
+        res[i] = 48 + (x % 10) as u8;
+        x = x / 10;
+    }
+    res
 }
