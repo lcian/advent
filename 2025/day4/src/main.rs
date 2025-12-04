c22eba0c430267d305b29f44ec45c2db5c83de9f
lcian <17258265+lcian@users.noreply.github.com>
2025-12-04 20:13:51 +0100

    2025: day 4 problem 2

--- 2025/day4/src/main.rs
+++ 2025/day4/src/main.rs
@@ -49,7 +49,55 @@ fn problem1() {
 }
 
 fn problem2() {
-    let input = include_str!("example.txt");
+    let input = include_str!("input.txt");
+    const N: usize = 140;
+    let mut m: [[i8; N]; N] = [[0; N]; N];
+    for (i, line) in input.lines().enumerate() {
+        for j in 0..line.len() {
+            if line[j..j + 1].contains("@") {
+                m[i][j] = 1;
+            }
+        }
+    }
     let mut res = 0;
+    let mut good = true;
+    while good {
+        good = false;
+        for i in 0..N {
+            for j in 0..N {
+                if m[i][j] == 0 {
+                    continue;
+                }
+                let i = i as i32;
+                let j = j as i32;
+                let mut count = 0;
+                for ii in i - 1..=i + 1 {
+                    for jj in j - 1..=j + 1 {
+                        if ii < 0
+                            || ii >= N as i32
+                            || jj < 0
+                            || jj >= N as i32
+                            || (i == ii && j == jj)
+                        {
+                            continue;
+                        }
+                        if m[ii as usize][jj as usize].abs() == 1 {
+                            count += 1;
+                        }
+                    }
+                }
+                if count < 4 {
+                    good = true;
+                    m[i as usize][j as usize] = -1;
+                    res += 1;
+                }
+            }
+        }
+        for i in 0..N {
+            for j in 0..N {
+                m[i][j] = m[i][j].max(0);
+            }
+        }
+    }
     println(res);
 }
